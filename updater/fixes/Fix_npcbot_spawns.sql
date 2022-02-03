eefeb0a360fef3e0213bb4af08569d7b68fb7a29
Juggalo187 <skulyspam@gmail.com>
2022-02-03 14:35:43 -0600

    ..

--- updater/fixes/Fix_npcbot_spawns.sql
+++
@@ -1,4 +0,0 @@
-UPDATE creature SET Map = 1 WHERE id > 70000 and id < 70466;
-UPDATE creature SET Map = 1 WHERE id > 70550 and id < 70556;
-UPDATE creature SET Map = 1 WHERE id > 70556 and id < 70562;
-UPDATE creature SET Map = 1 WHERE id > 70562 and id < 70573;
--- updater/sql/updates/characters/Delete_Bots_no_owner1.sql
+++
@@ -1,5 +0,0 @@
-delete from `characters_npcbot` where `owner` = 0 and `entry` > 70000 and `entry` < 70466;
-delete from `characters_npcbot` where `owner` = 0 and `entry` > 70550 and `entry` < 70556;
-delete from `characters_npcbot` where `owner` = 0 and `entry` > 70556 and `entry` < 70562;
-delete from `characters_npcbot` where `owner` = 0 and `entry` > 70562 and `entry` < 70573;
-delete from `characters_npcbot` where `owner` = 0 and `entry` > 70574 and `entry` < 70580;
